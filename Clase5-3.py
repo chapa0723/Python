9eb3544074f9d3bb70fd35950c67a6835066aaf2
Chapa <dchaparro0723@gmail.com>
2022-03-25 00:28:39 -0300

    Clase5-3

---
+++ Clase5-3.py
@@ -0,0 +1,13 @@
+#Escribe una función que pueda decirte si un año (número entero) es bisiesto o no.
+
+def bisiesto(anio):
+    if anio % 4 == 0:
+        if anio % 100 == 0:
+            if anio % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
