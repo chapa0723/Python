54b05642880fbe7867df3cee705ea4140ec6aefc
Chapa <dchaparro0723@gmail.com>
2022-03-26 13:02:43 -0300

    Clase7-1

---
+++ Clase7-1.py
@@ -0,0 +1,14 @@
+import calculadora
+suma = calculadora.sumar(10, 20)
+resta = calculadora.restar(10, 20)
+multiplicacion = calculadora.multiplicar(10, 20)
+division = calculadora.dividir(10, 20)
+print("Funcion suma", suma)
+print("Funcion resta", resta)
+print("Funcion multiplicacion", multiplicacion)
+print("funcion divicion", division)
+
+
+
+
+
---
+++ calculadora.py
@@ -0,0 +1,11 @@
+#En este ejercicio tendréis que crear un módulo que contenga las operaciones básicas de una calculadora: sumar, restar, multiplicar y dividir.
+#Este módulo lo importaréis a un archivo python y llamaréis a las funciones creadas. Los resultados tenéis que mostrarlos por consola.
+
+def sumar(num1, num2):
+    return num1 + num2
+def restar(num1, num2):
+    return num1 - num2
+def multiplicar(num1, num2):
+    return num1 * num2
+def dividir(num1, num2):
+    return num1 / num2
